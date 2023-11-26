a5b0658a7750dc4670395bb4e81039303f1f5d16
CotswoldsMaker <mark.allan.bailey@gmail.com>
2023-11-26 21:14:01 +0000

    Testing commits and pushes via python

--- app/cshd/app/functions/git_control.py
+++ app/cshd/app/functions/git_control.py
@@ -12,15 +12,18 @@ import pexpect
 
 class GitController:
     def __init__(
-        self, user_org: str | None = None, token: str | None = None
+        self,
+        repo_path: str | None = None,
+        user_org: str | None = None,
+        token: str | None = None,
     ) -> None:
         if user_org == None and token == None:
             # TODO - will need to change find_dotenv() to live .env location
             dot_values = dotenv_values(find_dotenv())
+            self.repo_path = repo_path
+            # TODO - will need to handle None case
             self.user_org = dot_values.get("user_org")
-            print(self.user_org)
             self.token = dot_values.get("github_token")
-            print(self.token)
         else:
             raise FileNotFoundError(f"To be determined.")
 
@@ -34,46 +37,32 @@ class GitController:
 
         return repos_found
 
-    def commit_and_push(self) -> bool:
-        # Path to the local repository
-        repo_path = "/cshd"
-        # Open the repository
-        repo = Repo(repo_path)
-        print(1)
-        # Stage all changes
+    def commit_and_push(self, verbose: bool = False) -> bool:
+        repo = Repo(self.repo_path)
         repo.git.add("--all")
-        print(2)
-        # Commit the changes
+
         commit_message = "Testing commits and pushes via python"
         repo.git.commit("-m", commit_message)
-        print(3)
-        # Push the changes to the remote repository
-        # repo.remote().push()
+
         origin = repo.remote(name="origin")
-        # origin.push()
         child = pexpect.spawn("git push", timeout=10)
-        # print(child.readline())
         child.expect("Username for 'https://github.com': ")
         child.sendline(self.user_org)
         child.expect(f"Password for 'https://{ self.user_org }@github.com': ")
         child.sendline(self.token)
-        # child.wait()
-        output = child.readline()
-        while b"\r\n" in output:
-            print(output)
-            output = child.readline()
-        # print(child.read())
-        # print(child.before)
-        # child.wait()ff
-        print(4)
 
-        # asdjflksajhssssggg11s
-        print("Changes committed and pushed successfully.")
+        if verbose:
+            output = child.readline()
+            while b"\r\n" in output:
+                print(output)
+                output = child.readline()
+        else:
+            child.wait()
         return True
 
 
 if __name__ == "__main__":
     print("Starting...")
-    gc = GitController()
+    gc = GitController("/cshd")
     gc.commit_and_push()
     # print(gc.get_repos())
